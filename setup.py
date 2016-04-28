ecc6019b7282caa093bcd897d10bc5462d20b005
Alain IVARS <alainivars@gmail.com>
2016-04-28 17:16:41 +0200

    fix depend for reCapcha in setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 install_requires = [
     'django-crispy-bootstrap>=0.1.1.1',
     'django-crispy-forms-ng==2.0.0',
-    'django-recaptcha==1.0.5'
+    'django-recaptcha==1.0.5',
     'requests>=2.7.0',
 ]
 
